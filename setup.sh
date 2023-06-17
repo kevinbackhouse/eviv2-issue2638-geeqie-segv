2d419afe4141848288804c3127b1a9964adcdc6a
Kevin Backhouse <kevinbackhouse@github.com>
2023-06-17 22:25:31 +0000

    fix build

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ cd exiv2
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .
-sudo cmake --install .
-cd ..
+make
+sudo make install
+cd ../..
 
 cd geeqie
-git apply ../geeqie-patch.txt
 meson setup build
-ninja -C build install
+ninja -C build
+sudo ninja -C build install
 cd ..
