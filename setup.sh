7c3dc4b6806ae3215aac63d7d6efa934a0172245
Kevin Backhouse <kevinbackhouse@github.com>
2023-06-17 21:38:51 +0000

    Build geeqie

--- setup.sh
+++ setup.sh
@@ -3,9 +3,15 @@
 git submodule update --init
 
 cd exiv2
-./ci/install_dependencies.sh
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
-cmake --install .
+sudo cmake --install .
+cd ..
+
+cd geeqie
+git apply ../geeqie-patch.txt
+meson setup build
+ninja -C build install
+cd ..
